b088b0df62305231b5d3a0cdeba996b795164021
williamEstony <williamestony@gmail.com>
2019-11-02 19:22:09 -0400

    My first commit

---
+++ programMain.java
@@ -0,0 +1,13 @@
+
+public class programMain {
+
+	public static void main(String[] args) {
+		
+		Athlete Stephen = new Athlete("Stephen Tyler", "https://www.tfrrs.org/athletes/6424193.html");
+		
+		
+		
+		
+	}
+
+}
