9d807359965e0721c9caa482d6791fe7cec76f8c
Erkin <erkin.verbeek@trincoll.edu>
2019-12-29 15:54:02 +0100

    Revamped UI Constraint system to be more modular and flexible.

--- guiPackage/UIConstraint.java
+++ guiPackage/UIConstraint.java
@@ -9,12 +9,24 @@ public class UIConstraint {
 	
 	protected ConstraintType type;	// saves what type of constraint this is
 	protected double value;			// saves the value that this constraint uses
+	protected UIConstraintSet referenceConstraints;
+	// if no reference frame:
 	// a negative value means subtracted from the right wall, a positive value means from the left wall
 	
-	public UIConstraint(ConstraintType constraintType, double constraintValue) {
-		this.type = constraintType;
-		// error checking
-		if (type == ConstraintType.relative && (constraintValue > 1 || constraintValue < 0)) {
+	// two types of constraints, fixed and relative
+	// relative only requires the constraint value
+	// fixed requires constraint value and reference constraints to be based off of
+	//		if reference frame is null, then screen edges are reference constraints
+	public UIConstraint(double constraintValue, UIConstraintSet referenceConstraints) {
+		this.type = ConstraintType.fixed;
+		this.referenceConstraints = referenceConstraints;
+		this.value = constraintValue;
+	}
+	public UIConstraint(double constraintValue) {
+		this.type = ConstraintType.relative;
+		this.referenceConstraints = null;
+		// error checking constraint value
+		if (constraintValue > 1 || constraintValue < 0) {
 			System.out.println("Constraint Creation Error: did not get expected value between 0 and 1.");
 			this.value = 0;
 		} else {
--- guiPackage/UIConstraintSet.java
+++ guiPackage/UIConstraintSet.java
@@ -13,14 +13,32 @@ public class UIConstraintSet {
 		this.yConstraint = yConstraint;
 	}
 	
+	
+	// creates a clone of this set but with the given offsets
+	public UIConstraintSet createOffsetContraintSet(double xOffset, double yOffset) {
+		// creates a new fixed xConstraint that is xOffset away from the current xConstraint
+		UIConstraint newXConstraint = new UIConstraint(xConstraint.value + xOffset, this);
+		UIConstraint newYConstraint = new UIConstraint(yConstraint.value + yOffset, this);
+		
+		// hostFrame reference can be "null" because we never actually reference it
+		// because our new clone constraint set is of type "fixed"
+		return new UIConstraintSet(null, newXConstraint, newYConstraint);
+	}
+	
+	
 	// returns the center x position of this object as defined by the xConstraint
 	public double getX() {
 		switch (xConstraint.type) {
 			case fixed:	// if fixed constraint
-				if (xConstraint.value >= 0)		// if value positive
-					return xConstraint.value;	// then place as distance from left wall
-				else							// if value negative
-					return hostFrame.getWidth() + xConstraint.value;	// then place as distance from right wall
+				// if no reference object, then constraint is based off of frame walls
+				if (xConstraint.referenceConstraints == null) {
+					if (xConstraint.value >= 0)		// if value positive
+						return xConstraint.value;	// then place as distance from left wall
+					else							// if value negative
+						return hostFrame.getWidth() + xConstraint.value;	// then place as distance from right wall
+				} // else our position is based off distance from a reference object
+				return xConstraint.referenceConstraints.getX() + xConstraint.value;
+				
 			case relative:	// if relative constraint
 				return hostFrame.getWidth() * xConstraint.value;		// then place as percentage of width
 			default:	// needed to suppress error
@@ -32,15 +50,19 @@ public class UIConstraintSet {
 	public double getY() {
 		switch (yConstraint.type) {
 			case fixed:	// if fixed constraint
-				if (yConstraint.value >= 0)		// if value positive
-					return yConstraint.value;	// then place as distance from top wall
-				else							// if value negative
-					return hostFrame.getHeight() + yConstraint.value;	// then place as distance from bottom wall
+				// if no reference object, then constraint is based off of frame walls
+				if (xConstraint.referenceConstraints == null) {
+					if (yConstraint.value >= 0)		// if value positive
+						return yConstraint.value;	// then place as distance from top wall
+					else							// if value negative
+						return hostFrame.getHeight() + yConstraint.value;	// then place as distance from bottom wall
+				} // else our position is based off distance from a reference object
+				return yConstraint.referenceConstraints.getY() + yConstraint.value;
+				
 			case relative:	// if relative constraint
 				return hostFrame.getHeight() * yConstraint.value;		// then place as percentage of height
 			default:	// needed to suppress error
 				return 0;
 		}
 	}
-	
 }
