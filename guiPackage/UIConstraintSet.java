274fcbf667cacae3365e818d8ec611101ac17f50
Erkin <erkin.verbeek@trincoll.edu>
2020-01-11 23:30:47 +0100

    added simplest all-in-one constraint constructor. Constraint Sets now work well with width and height constraints

--- guiPackage/UIConstraintSet.java
+++ guiPackage/UIConstraintSet.java
@@ -20,14 +20,25 @@ import java.awt.Frame;
  */
 
 
+/*
+ * first you call one of the constructors,
+ * then you add extra constraints as desired, which will add them to wall that is closest to the provided reference constraint
+ */
+
+
 
 public class UIConstraintSet {
 	private UIConstraint topConstraint, bottomConstraint, leftConstraint, rightConstraint;
-	private static UIConstraint topWall, bottomWall, leftWall, rightWall;
+	public static UIConstraint topWall, bottomWall, leftWall, rightWall;
+	private static Frame hostFrame;
+	private double width = -1, height = -1;	// extra optional constraints, constraints take priority over these
 	
-	// initializes all the wall constraints, only once
-	private UIConstraintSet(Frame hostFrame) {
-		if (topWall == null) {
+	// initializes all the wall constraints, only once, until a new host frame is given, then
+	// the host frame is reset and the wall constraints are rebuilt
+	public UIConstraintSet(Frame hostFrame) {
+		// if a new frame is given
+		if (UIConstraintSet.hostFrame != hostFrame) {
+			UIConstraintSet.hostFrame = hostFrame;	// saves hostFrame reference
 			topWall = new UIConstraint(hostFrame, WALL.TOP);
 			bottomWall = new UIConstraint(hostFrame, WALL.BOTTOM);
 			leftWall = new UIConstraint(hostFrame, WALL.LEFT);
@@ -35,14 +46,91 @@ public class UIConstraintSet {
 		}
 	}
 	
-	// the most basic set, a rectangle with a fixed (x,y) position
+//	public UIConstraintSet(Frame hostFrame, double width, double height) {
+//		this(hostFrame);
+//		this.width = width;
+//		this.height = height;
+//	}
+	
+	// probably the easiest constructor to use, doesn't require anything more to be set
 	public UIConstraintSet(Frame hostFrame, double xPos, double yPos, double width, double height) {
 		this(hostFrame);
-		topConstraint = new UIConstraint(yPos, topWall);
-		bottomConstraint = new UIConstraint(height,topConstraint);
-		leftConstraint = new UIConstraint(xPos, leftWall);
-		rightConstraint = new UIConstraint(width, leftConstraint);
+		setLeftConstraint(xPos);
+		setTopConstraint(yPos);
+		this.width = width;
+		this.height = height;
 	}
+	
+	
+	public void addConstraint(double constraintVal, UIConstraint referenceConstraint) {
+		
+		// calculate which wall is closest to the reference constraint
+		// reference constraint should be
+	}
+	
+	public void addConstraint(double constraintVal, UIConstraintSet referenceConstraintSet) {
+		// same thing^^ but figure out which wall of the reference also set to use
+	}
+	
+	
+	
+	
+	/* constraint get methods to allow the creation of constraints that reference other constraints */
+	public UIConstraint getTopConstraint() { return topConstraint; }
+	public UIConstraint getBottomConstraint() { return bottomConstraint; }
+	public UIConstraint getLeftConstraint() { return leftConstraint; }
+	public UIConstraint getRightConstraint() { return rightConstraint; }
+	
+	/* the simpler constraints */
+	public void setWidth(double newWidth) {
+		this.width = newWidth;
+	}
+	public void setHeight(double newHeight) {
+		this.height = newHeight;
+	}
+	
+	/* all possible constraint update/set/reset methods */
+	public void setTopConstraint(double constraintVal) {
+		this.topConstraint = new UIConstraint(constraintVal, topWall);
+	}
+	public void setTopConstraint(double constraintVal, UIConstraint referenceConstraint1) {
+		this.topConstraint = new UIConstraint(constraintVal, referenceConstraint1);
+	}
+	public void setTopConstraint(double constraintVal, UIConstraint referenceConstraint1, UIConstraint referenceConstraint2) {
+		this.topConstraint = new UIConstraint(constraintVal, referenceConstraint1, referenceConstraint2);
+	}
+	public void setBottomConstraint(double constraintVal) {
+		this.bottomConstraint = new UIConstraint(constraintVal, bottomWall);
+	}
+	public void setBottomConstraint(double constraintVal, UIConstraint referenceConstraint1) {
+		this.bottomConstraint = new UIConstraint(constraintVal, referenceConstraint1);
+	}
+	public void setBottomConstraint(double constraintVal, UIConstraint referenceConstraint1, UIConstraint referenceConstraint2) {
+		this.bottomConstraint = new UIConstraint(constraintVal, referenceConstraint1, referenceConstraint2);
+	}
+	public void setLeftConstraint(double constraintVal) {
+		this.leftConstraint = new UIConstraint(constraintVal, leftWall);
+	}
+	public void setLeftConstraint(double constraintVal, UIConstraint referenceConstraint1) {
+		this.leftConstraint = new UIConstraint(constraintVal, referenceConstraint1);
+	}
+	public void setLeftConstraint(double constraintVal, UIConstraint referenceConstraint1, UIConstraint referenceConstraint2) {
+		this.leftConstraint = new UIConstraint(constraintVal, referenceConstraint1, referenceConstraint2);
+	}
+	public void setRightConstraint(double constraintVal) {
+		this.rightConstraint = new UIConstraint(constraintVal, rightWall);
+	}
+	public void setRightConstraint(double constraintVal, UIConstraint referenceConstraint1) {
+		this.rightConstraint = new UIConstraint(constraintVal, referenceConstraint1);
+	}
+	public void setRightConstraint(double constraintVal, UIConstraint referenceConstraint1, UIConstraint referenceConstraint2) {
+		this.rightConstraint = new UIConstraint(constraintVal, referenceConstraint1, referenceConstraint2);
+	}
+	
+	
+	
+	
+	
 	
 	// any parameter (excluding the hostFrame param) can be accompanied by another constrainable object,
 	// which becomes the reference constraint for that param
@@ -52,11 +140,15 @@ public class UIConstraintSet {
 	// like this?:
 	// public void setLeftConstraint(double constraintVal, Constrainable referenceObject);
 	
-	// Do I want an abstract Constrainable class? and Drawable would extend Constrainable, TeamJList would also extend Constrainable
+	// Do I want an abstract Constrainable class/interface? and Drawable would extend Constrainable, TeamJList would also extend Constrainable
 	
 	
 	
+	// every constraint set needs two of three to be set:
+	// leftConstraint, rightConstraint, width
 	
+	// and two of these three to be set also
+	// topConstraint, bottomConstraint, height
 	
 	
 	
@@ -65,27 +157,57 @@ public class UIConstraintSet {
 	/* returns the calculated values of the constraint set */
 	// returns x value of left of object
 	public double getX() {
-		return leftConstraint.computeVal();
+		// figures out which side of the object has a constraint set
+		// favoring left side first, then right side & width
+		if (leftConstraint != null)
+			return leftConstraint.computeVal();
+		else	// else leftConstraint is null so other two are set
+			return rightConstraint.computeVal() - width;
 	}
 	// returns y value of top of object
 	public double getY() {
-		return topConstraint.computeVal();
+		if (topConstraint != null)
+			return topConstraint.computeVal();
+		else
+			return bottomConstraint.computeVal() - height;
 	}
 	// returns x value of center of object
 	public double getCenterX() {
-		return (leftConstraint.computeVal() + rightConstraint.computeVal())/2;
+		// ignores width setting, prioritizing constraints first
+		if (rightConstraint != null) {
+			if (leftConstraint != null) {
+				// both constraints are not null
+				return (rightConstraint.computeVal() + leftConstraint.computeVal())/2;
+			} else	// left constraint is null
+				return rightConstraint.computeVal() - (width/2);
+		} else // right constraint is null
+			return leftConstraint.computeVal() + (width/2);
 	}
 	// returns y value of center of object
 	public double getCenterY() {
-		return (topConstraint.computeVal() + bottomConstraint.computeVal())/2;
+		// ignores width setting, prioritizing constraints first
+		if (topConstraint != null) {
+			if (bottomConstraint != null) {
+				// both constraints are not null
+				return (bottomConstraint.computeVal() + bottomConstraint.computeVal())/2;
+			} else	// bottom constraint is null
+				return topConstraint.computeVal() + (height/2);
+		} else // top constraint is null
+			return bottomConstraint.computeVal() - (height/2);
 	}
-	// returns height of object
+	// calculate and returns height of object, favoring constraints over height variable
 	public double getHeight() {
-		return bottomConstraint.computeVal() - topConstraint.computeVal();
+		if (bottomConstraint != null && topConstraint != null)	// if both constraints are non-null
+			return bottomConstraint.computeVal() - topConstraint.computeVal();
+		else
+			return height;
 	}
 	// returns width of object
 	public double getWidth() {
-		return rightConstraint.computeVal() - leftConstraint.computeVal();
+		if (leftConstraint != null && rightConstraint != null)	// if both constraints are non-null
+			return rightConstraint.computeVal() - leftConstraint.computeVal();
+		else
+			return width;
 	}
 	
 	
@@ -105,49 +227,6 @@ public class UIConstraintSet {
 		// because our new clone constraint set is of type "fixed"
 		return new UIConstraintSet(null, newXConstraint, newYConstraint);
 	}
-	
-	
-	// returns the center x position of this object as defined by the xConstraint
-	public double getX() {
-		switch (xConstraint.type) {
-			case fixed:	// if fixed constraint
-				// if no reference object, then constraint is based off of frame walls
-				if (xConstraint.referenceConstraints == null) {
-					if (xConstraint.value >= 0)		// if value positive
-						return xConstraint.value;	// then place as distance from left wall
-					else							// if value negative
-						return hostFrame.getWidth() + xConstraint.value;	// then place as distance from right wall
-				} // else our position is based off distance from a reference object
-				return xConstraint.referenceConstraints.getX() + xConstraint.value;
-				
-			case relative:	// if relative constraint
-				return hostFrame.getWidth() * xConstraint.value;		// then place as percentage of width
-			default:	// needed to suppress error
-				return 0;
-		}
-	}
-	
-	// returns the center y position of this object as defined by the yConstraint
-	public double getY() {
-		switch (yConstraint.type) {
-			case fixed:	// if fixed constraint
-				// if no reference object, then constraint is based off of frame walls
-				if (xConstraint.referenceConstraints == null) {
-					if (yConstraint.value >= 0)		// if value positive
-						return yConstraint.value;	// then place as distance from top wall
-					else							// if value negative
-						return hostFrame.getHeight() + yConstraint.value;	// then place as distance from bottom wall
-				} // else our position is based off distance from a reference object
-				return yConstraint.referenceConstraints.getY() + yConstraint.value;
-				
-			case relative:	// if relative constraint
-				return hostFrame.getHeight() * yConstraint.value;		// then place as percentage of height
-			default:	// needed to suppress error
-				return 0;
-		}
-	}
-	
-	
 	*/
 	// only used for Wall Constraints
 	private enum WALL {
